8155db3e13e4f182158d3b273ec867fa3b72de42
charley-padron <33139667+charley-padron@users.noreply.github.com>
2019-03-08 20:51:13 -0500

    User class and its interface and read athlete file
    Finished user class and it's interface.
    Created a python program that reads the csv file containing athlete information and stores it to firestore

---
+++ user.java
@@ -0,0 +1,17 @@
+package com.example.charley.riooms;
+
+public interface user {
+
+    void setName(String name);
+    void setPhoneNum(String phoneNum);
+    void setAge(int age);
+    void setEmail(String email);
+    String getName();
+    String getPhoneNum();
+    int getAge();
+    String getEmail();
+    ArrayList<Ticket> getTickets();
+    void buyTickets(Event e);
+    void cancelTicket(Ticket t);
+
+}
