5e7aec7500ca7dcea5b6a768545f7441df4d54c7
Bilal <noreldenb1@montclair.edu>
2019-04-17 00:03:58 -0400

    pc changes

---
+++ ANDROID_STUFF/functions/index.js
@@ -0,0 +1,20 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.myFunctionName = functions.firestore
+    .document('EVENTS/Men\'s 200m Backstroke').onWrite((change, context) => {
+        // ... Your code here
+        console.log("Test");
+        
+
+    });
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
--- firebaseFunc/functions/index.js
+++ firebaseFunc/functions/index.js
@@ -1,4 +1,17 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.myFunctionName = functions.firestore
+    .document('EVENTS/Men\'s 200m Backstroke').onWrite((change, context) => {
+        // ... Your code here
+        console.log("Test");
+        
+
+    });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
