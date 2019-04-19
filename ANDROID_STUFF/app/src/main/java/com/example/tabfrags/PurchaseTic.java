11df57d48a6f1b693b17aff88e95bf359d15f42c
Bilal <noreldenb1@montclair.edu>
2019-04-19 01:17:56 -0400

    Changes + Update on UI
    Need to work on the cardviews, add more data in db and do filters. and emlpoyee update events, register athletes maybe? and athlete schedule AG sessions.

--- ANDROID_STUFF/app/src/main/java/com/example/tabfrags/PurchaseTic.java
+++ ANDROID_STUFF/app/src/main/java/com/example/tabfrags/PurchaseTic.java
@@ -1,5 +1,6 @@
 package com.example.tabfrags;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
@@ -7,7 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -31,49 +37,96 @@ public class PurchaseTic extends AppCompatActivity {
 
     FirebaseFirestore fs;
     String holds2 = swapS;
-
+    private Button btn;
+    private EditText etext;
+    LinearLayout LL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        LL = findViewById(R.id.LL);
+
 
 
         fs = FirebaseFirestore.getInstance();
+        btn= new Button(this);
 
         final DocumentReference fsref = fs.collection("EVENTS").document(holds2); //works
 
 
 
+        btn.setText("purchase");
+
+        LL.addView(btn);
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)btn.getLayoutParams();
+        params.setMargins(850, 200, 200, 0);
+        btn.setLayoutParams(params);
+
+        etext = new EditText(this);
+
+        etext.setPadding(250,600,200,50);
 
 
-        fsref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
 
-            Map<String, Object> seatsdec = new HashMap<>();
 
-            @RequiresApi(api = Build.VERSION_CODES.O)
+        LL.addView(etext);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
+            public void onClick(View v) {
 
-                    DocumentSnapshot doc = task.getResult();
+                fsref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+                    Map<String, Object> seatsdec = new HashMap<>();
+
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        String ticcounts = etext.getText().toString(); //input to string
+                        int ticcount = parseInt(ticcounts);             // input string to int
+                        if (task.isSuccessful()) {
+
+                            DocumentSnapshot doc = task.getResult();
+                            // make a dynamic button to confirm and edit text for amount
 
 
-                    int seatsint = parseInt(doc.getString("seats"));
-                    seatsint = seatsint-1;
-                    String seatsf = Integer.toString(seatsint);
-                    seatsdec.put("seats", seatsf);
+                            int seatsint = parseInt(doc.getString("seats"));
+                            if (ticcount > seatsint){
+
+                                Toast.makeText(getApplicationContext(), "Cannot do that, not enough tickets" ,
+                                        Toast.LENGTH_LONG).show();
+                                etext.getText().clear();
+                            }
+                            else if (seatsint <=0){
+                                btn.setVisibility(View.INVISIBLE);
+                                Toast.makeText(getApplicationContext(), "TICKETS ARE SOLD OUT" ,
+                                        Toast.LENGTH_LONG).show();
+
+                            }
+
+                            else {
+                                seatsint = seatsint - ticcount;
+                                String seatsf = Integer.toString(seatsint);
+                                seatsdec.put("seats", seatsf);
+
+                                 Toast.makeText(getApplicationContext(), "Your purchased "+ ticcount + " and there is "+ seatsf +"Tickets left  !!!" ,
+                                       Toast.LENGTH_LONG).show();
 
 
-                    Toast.makeText(getApplicationContext(), "Tickets left "+ seatsf+" !!!" ,
-                            Toast.LENGTH_LONG).show();
+                                fsref.set(seatsdec, SetOptions.merge());
+                            } }
 
-                   //fsref.set(seatsdec.toString(), SetOptions.merge());
-                    fsref.set(seatsdec, SetOptions.merge());
-                }
+                    }
+                });
+
+
 
             }
         });
+
 
     }
 
---
+++ RioOms/app/src/main/java/com/example/riooms/activityThree.java
@@ -0,0 +1,109 @@
+package com.example.riooms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class activityThree extends AppCompatActivity {
+    private TextView tt;
+    LinearLayout linearLayout;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+
+        linearLayout = (LinearLayout) findViewById(R.id.L4);
+        //tt = findViewById(R.id.tv1);
+
+
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //TextView tv=new TextView(this);
+        tt = new TextView(this);
+        tt.setText("TESTING ACTIVITY 3");
+        tt.setLayoutParams(lparams);
+        //tv.setText("test");
+        linearLayout.addView(tt);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent intent0 = new Intent(activityThree.this,MainActivity.class);
+                        startActivity(intent0);
+
+                        return true;
+                    case R.id.navigation_Filter:
+                        Intent intent1 = new Intent(activityThree.this,activityOne.class);
+                        startActivity(intent1);
+
+
+                        return true;
+                    case R.id.navigation_Tickets:
+                        Intent intent2 = new Intent(activityThree.this,activityTwo.class);
+                        startActivity(intent2);
+
+                        return true;
+                    case R.id.navigation_notifications:
+
+                        return true;
+                }
+                return false;
+
+            }
+        });
+        Menu menu = navigation.getMenu();
+        MenuItem menuItem = menu.getItem(3);
+        menuItem.setChecked(true);
+
+/*
+         BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+                = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent intent0 = new Intent(activityOne.this,MainActivity.class);
+                        startActivity(intent0);
+
+                        return true;
+                    case R.id.navigation_Filter:
+                        // mTextMessage.setPadding(0,0,0,0);
+                        // mTextMessage.setVisibility(View.INVISIBLE);       // alone sets it always to invisible
+                        //setContentView(R.layout.tab3event); switch to frag tab
+                        //setContentView(R.id.Linear3);     //figure this out
+                        //inActivity.this, activityOne.class);
+                        // startActivity(intent);
+                        // mTextMessage.setText(R.string.title_Filter);
+                        //mTextMessage.setVisibility(View.INVISIBLE);
+                        // here goes the code for filter
+                        return true;
+                    case R.id.navigation_Tickets:
+
+                        return true;
+                    case R.id.navigation_notifications:
+
+                        return true;
+                }
+                return false;
+            }
+        }; */
+
+
+
+    }
+
+
+
+
+}
