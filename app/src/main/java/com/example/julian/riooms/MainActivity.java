21b350ad956b8943b67701a2cbf729c017fa1043
juliansangillo <44471531+juliansangillo@users.noreply.github.com>
2019-04-26 00:19:19 -0400

    Add SignUp.java, Update MainActivity.java

--- app/src/main/java/com/example/julian/riooms/MainActivity.java
+++ app/src/main/java/com/example/julian/riooms/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.julian.riooms;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
@@ -48,10 +59,140 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-
+        switch(v.getId()) {
+            case R.id.login_action:
+                userLogin();
+                break;
+            case R.id.signup_action:
+                signUp();
+                break;
+            case R.id.cont_guest:
+                guestLogin();
+                break;
+        }
 
     }
 
+    public void userLogin() {
 
+        try {
+            email = emailField.getText().toString();
+            password = passwordField.getText().toString();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        String currUser = mAuth.getCurrentUser().getUid();
+
+                        userRef = db.collection("USER").document(currUser);
+                        athRef = db.collection("ATHLETE").document(currUser);
+                        empRef = db.collection("EMPLOYEE").document(currUser);
+
+                        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+
+                                    if (document.exists()) {
+                                        //todo User View Activities
+                                        Toast.makeText(MainActivity.this, "User Logs In!", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Log.d("Doc error", "error in finding doc");
+                                    }
+                                } else {
+                                    Log.d("Doc error", "Error in oncomplete doc listener");
+                                }
+
+                            }
+
+                        });
+
+                        athRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+
+                                    if (document.exists()) {
+                                        //todo toast that this works
+                                    } else {
+                                        Log.d("Doc error", "error in finding doc");
+                                    }
+                                } else {
+                                    Log.d("Doc error", "Error in oncomplete doc listener");
+                                }
+
+                            }
+
+                        });
+
+                        empRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+
+                                    if (document.exists()) {
+                                        //todo toast that this works
+                                    } else {
+                                        Log.d("Doc error", "error in finding doc");
+                                    }
+                                } else {
+                                    Log.d("Doc error", "Error in oncomplete doc listener");
+                                }
+
+                            }
+
+                        });
+                    } else
+                        Toast.makeText(MainActivity.this, "Incorrect email or password", Toast.LENGTH_LONG).show();
+
+                }
+
+            });
+        }
+        catch(IllegalArgumentException a) {
+            Toast.makeText(MainActivity.this, "Please enter email and password to log in", Toast.LENGTH_LONG).show();
+        }
+        catch(Exception e) {
+            Toast.makeText(MainActivity.this, "Something went wrong. Please try again later", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void signUp() {
+
+        startActivity(new Intent(getApplicationContext(), SignUp.class));
+
+    }
+
+    public void guestLogin() {
+
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    //todo Make app compatible for anonymous login
+                    Toast.makeText(MainActivity.this, "Guest Login Works!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Something went wrong. Please try again later", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+    }
 
 }
---
+++ app/src/main/java/com/example/julian/riooms/SignUp.java
@@ -0,0 +1,121 @@
+package com.example.julian.riooms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import Source.InputException;
+import Source.User;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+
+    private CollectionReference userRef;
+
+    EditText emailField, passwordField, confirmPassField, nameField, phoneField, ageField;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        FirebaseFirestore db;
+
+        mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance();
+        userRef = db.collection("USER");
+
+        emailField = findViewById(R.id.signup_email_field);
+        passwordField = findViewById(R.id.signup_password_field);
+        confirmPassField = findViewById(R.id.confirm_password_field);
+
+        nameField = findViewById(R.id.name_field);
+        phoneField = findViewById(R.id.phone_field);
+        ageField = findViewById(R.id.age_field);
+
+        register = findViewById(R.id.register_action);
+        register.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+            case R.id.register_action:
+                register();
+                break;
+        }
+
+    }
+
+    public void register() {
+
+        final String email, password, confirmPass, name, phoneNum;
+        final int age;
+
+        try {
+            email = emailField.getText().toString();
+            password = passwordField.getText().toString();
+            confirmPass = confirmPassField.getText().toString();
+            name = nameField.getText().toString();
+            phoneNum = phoneField.getText().toString();
+            age = Integer.parseInt(ageField.getText().toString());
+
+            if (password.equals(confirmPass)) {
+                try {
+                    final User user = new User(name, phoneNum, age, email);
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                        String id;
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                id = task.getResult().getUser().getUid();
+                                userRef.document(id).set(user);
+
+                                Toast.makeText(SignUp.this, "Welcome " + name + ". Please log in", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            } else {
+                                Toast.makeText(SignUp.this, "" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+
+                    });
+                }
+                catch (InputException i) {
+                    Toast.makeText(SignUp.this, "" + i, Toast.LENGTH_LONG).show();
+                }
+            } else
+                Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_LONG).show();
+        }
+        catch(IllegalArgumentException a) {
+            Toast.makeText(SignUp.this, "Some fields are empty. Please fill out all fields", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(SignUp.this, "Something went wrong. Please try again later" + e, Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+}
